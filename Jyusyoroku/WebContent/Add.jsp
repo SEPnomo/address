51f675c68e417022cc58a558c41bbb6a57adc036
nomonaoki1193 <nomonaoki1193@DESKTOP-98CKBV9.ad.se-project.co.jp>
2020-06-22 18:14:43 +0900

    エラーチェック追加

--- Jyusyoroku/WebContent/Add.jsp
+++ Jyusyoroku/WebContent/Add.jsp
@@ -6,7 +6,7 @@
 <meta charset="UTF-8">
 <title>Jyushoroku</title>
 <style>
-.Sam
+.Sam{
 }
 </style>
 </head>
@@ -23,15 +23,6 @@
 			<td>　　　番号:<input type="text" name="tel" size="20"></td>
 		</tr>
 	</table>
-	<div class="Sample">
-		　　　カテゴリ：<select name="categoryid">
-			<option value="1">家族</option>
-			<option value="2">親戚</option>
-			<option value="3">知人</option>
-			<option value="4">仕事</option>
-			<option value="5">その他</option>
-		</select>
-	</div>
 	<div>
 		　　　<input type="submit" style="width: 7%;" value="確認">　　<input
 			type="submit" style="width: 7%;" value="戻る">
--- Jyusyoroku/src/pkg/AddBL.java
+++ Jyusyoroku/src/pkg/AddBL.java
@@ -1,6 +1,7 @@
 package pkg;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,19 +14,47 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/AddBL")
 public class AddBL extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AddBL() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public AddBL() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		//変数の宣言.jspから受け取る
+		String name = request.getParameter("name");
+		String address = request.getParameter("name");
+		String tel = request.getParameter("name");
+		String errmsg = "";
+
+		//定数の宣言
+		final String  ERRMSG_NAME01="名前は全角20文字以内で入力してください";
+		final String  ERRMSG_NAME02="名前は必須項目です";
+		final String  ERRMSG_ADDRESS01="住所は全角40文字以内で入力してください";
+		final String ERRMSG_ADDRESS02="住所は必須項目です住所は必須項目です";
+		final String ERRMSG_TEL01="電話番号は「000-0000-0000」の形式で入力してください";
+
+		//
+		if(40 < name.getBytes().length) {
+			errmsg=ERRMSG_NAME01 +  "<BR>";
+		}else if (name.getBytes().length==0) {
+			errmsg=ERRMSG_NAME02 + "<BR>";
+		}else if (80 < address.getBytes().length) {
+			errmsg=ERRMSG_ADDRESS01 + "<BR>";
+		}else if (address.getBytes().length==0) {
+			errmsg=ERRMSG_ADDRESS02 + "<BR>";
+		}else if (tel.getBytes().length > 0 &&!(tel.matches("{0-9]{3}-[0-9]{4}-[0-9]{4}")));
+			errmsg=ERRMSG_TEL01 + "<BR>" ;
+        //リクエスト情報を作成
+//		request.
+
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
@@ -33,7 +62,8 @@ public class AddBL extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
